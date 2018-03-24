f43da37660d4070f580303b32b2ff8f081e438ee
frontjss <frontjss@gmail.com>
2018-03-24 17:57:58 +0300

    New

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def main():
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == "__main__":
-    app.run(host='http://domalogica.com/', port=8080)
+    app.run(host='http://domalogica.com/', port=80)
